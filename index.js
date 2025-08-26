70c732c826d1c96ef097fd6e65647864d92644ef
John98410 <johnnelson.dev@gmail.com>
2025-08-25 21:53:04 -0600

    router * add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const path = require('path');
 const express = require('express');
-const app = express();  
+const path = require('path');
+const app = express();
 require('dotenv').config(); // Loads .env
 
 const PORT = process.env.PORT || 80;
@@ -9,15 +9,11 @@ app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use('/api', require('./routers')); // Use the routers
 
-// 2️⃣ Serve static
 app.use(express.static(path.join(__dirname, 'build')));
 
-// 3️⃣ SPA catch-all
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-// Sample route
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 console.log('API Key:', process.env.OPENAI_API_KEY ? process.env.OPENAI_API_KEY : "Not API Key");
 
